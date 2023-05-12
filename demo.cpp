d131c69079a9a41ed34428e324095b600b5b1271
brown.liuzan <brown.liuzan@gmail.com>
2023-05-12 21:54:03 +0800

    Add cmake demo. Refine .gitignore to ignore Build*, CMackeCache.txt, etc.

---
+++ demo.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+#include "demo.h"
+
+namespace liuzan {
+namespace demo {
+	template<typename CharT, typename Char_Traits = std::char_traits<CharT>>
+	std::basic_ostream<CharT, Char_Traits>& operator <<(
+			std::basic_ostream<CharT, Char_Traits> &os,
+			const CmakeDemo & obj
+			)
+	{
+		os << obj.Major()
+		   << '.'
+		   << obj.Minor()
+		   << '.'
+		   << obj.Patch()
+		   << '.'
+		   << obj.Tweak();
+
+		return os;
+	}
+}
+}
+
+int main(int argc, char **argv)
+{
+	liuzan::demo::CmakeDemo vCMakeDemo{3, 26, 3};
+
+	std::cout << "cmake version: " << vCMakeDemo << std::endl;
+
+	return 0;
+}
